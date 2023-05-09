af39e090275fcf19e6477c4968dbc29a59cd33f1
hanuman262 <monahlawat@gmail.com>
2023-05-09 05:07:21 -0700

    created sample project for git

---
+++ src/test/java/bidfood/Practice.java
@@ -0,0 +1,25 @@
+package bidfood;
+/*
+import Page.*;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentReporter;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+
+import java.lang.reflect.Method;
+
+public class Practice 
+
+      // Reset the method name to blank when test method ends
+      BasePage.currentMethodName = null;
+
+   }
+
+   @Test
+   public void testSearch() {
+       GooglePage googlePage = new GooglePage(driver);
+       googlePage.search("selenium java");
+       Assert.assertTrue(driver.getTitle().contains("selenium java"));
+   }
+*/
